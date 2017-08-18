a06e31163fb2f37368957294b04073bc6c217c7a
johntalton <johntalton@gmail.com>
2017-08-18 10:54:08 -0700

    scrub

--- prog1/prog1.h
+++ prog1/prog1.h
@@ -6,28 +6,28 @@
 *   Draw routines for my graphis packedge.
 *      ¥NöP's Graphics Package
 *      This was originaly wirten for my cs470 graphics classs.
-*      it is not verry usefull for much else but it is good 
+*      it is not verry usefull for much else but it is good
 *      for learning the basics about how the primitive graphics
-*      work. 
+*      work.
 *
-*   John Talton 439.57.4799
+*   John Talton
 *   prog1 cs470 1/6/99
 *
 *   TODO
 *      get rid of all that damn BLANK space. there has got ot be a better
 *         way of makeing a array of .. well big.
-*      do some more documentation. Always nead that. 
+*      do some more documentation. Always nead that.
 *   HISTORY
 *      ver1.0 - Lines Complete.
-*      ver0.5 - more Line algorithms. Not user but 
+*      ver0.5 - more Line algorithms. Not user but
 *               good practic and I am trying to get my
 *               class work done. :)
 *      ver0.1 - basic primitive pixPlot and some lines
-*      
+*
 *   @version 1.0
 *   @author YNOP,jta001@coes.latech.edu,ynop@acm.org
 *   @copyright 1998,1999 The Abstract Company
-**************************************************************************/  
+**************************************************************************/
 //int Gcd(int a,int b){while(b!=0){int t=a;a=b;b=t;b=b % a;}return a;}//Euclid's GCD
 /**************************************************************************
 * stuff avalible in this ver
@@ -35,7 +35,7 @@
 
 /**************************************************************************
 *   horizLine
-*      this is a simple left to right hoizontal line drawing alg. it is 
+*      this is a simple left to right hoizontal line drawing alg. it is
 *      suposed to be sort and sweat so that it runs fast as all for our
 *      polygon fill - later.
 *   @param int x1 is the left x value
@@ -57,9 +57,9 @@ void horizLine(int x1,int x2, int y){
 *   @param int y2 is our y value of our second point
 *   @return void
 ***************************************************************************
-*   inv 
+*   inv
 *      a sting invers func used in bestFit .. is sucks for preformance
-*      be carefull is does not return a string it modifyes the original!!!      
+*      be carefull is does not return a string it modifyes the original!!!
 *   @param char *s is a string of any length to be reversed.
 *   @return void!!!
 **************************************************************************/
@@ -98,13 +98,13 @@ void bestFitLine(int x1, int y1, int x2, int y2){
          sub2 = strcat(sub2,sub1);
          x = x - y;
       }else if(x==y){
-         //finished 
+         //finished
       }else if(x < y){
          inv(sub1);
          sub1 = strcat(sub1,sub2);
          y = y - x;
       }
-   } 
+   }
    inv(sub2);
    strcat(sub2,sub1);
    int counter = 0;
@@ -115,21 +115,21 @@ void bestFitLine(int x1, int y1, int x2, int y2){
           case 1: pixPlot( y + x1,  x + y1);break;
           case 2: pixPlot(-x + x1 , y + y1);break;
           case 3: pixPlot(-y + x1 , x + y1);break;
-          case 4: pixPlot( x + x1 ,-y + y1);break;      
-          case 5: pixPlot( y + x1 ,-x + y1);break;           
-          case 6: pixPlot(-x + x1 ,-y + y1);break;     
-          case 7: pixPlot(-y + x1 ,-x + y1);break;         
+          case 4: pixPlot( x + x1 ,-y + y1);break;
+          case 5: pixPlot( y + x1 ,-x + y1);break;
+          case 6: pixPlot(-x + x1 ,-y + y1);break;
+          case 7: pixPlot(-y + x1 ,-x + y1);break;
        }
        if (sub2[counter] == '\0'){ counter = 0;}
        if (sub2[counter++] == '1'){ y++; }
        x++;
-   } 
+   }
    delete sub1;
    delete sub2;
 }
 /**************************************************************************
 *   simpleDDALine
-*      Uhhh could this be any slllooowwwerrr.. I just love floating point 
+*      Uhhh could this be any slllooowwwerrr.. I just love floating point
 *      numbers..
 *   @param int x1 is the x value of our first point
 *   @param int y1 is our y value of our first point
@@ -157,11 +157,11 @@ void simpleDDALine(int x1, int y1, int x2, int y2){
    }
 }
 /**************************************************************************
-*   bresLine 
+*   bresLine
 *      This is the one that we will actually use. it is pritty nice compared
-*      to the others here. It does a nifty math trick and figures out when 
+*      to the others here. It does a nifty math trick and figures out when
 *      to inc y or x. go look it up some place for explanation - its all over
-*      the net. 
+*      the net.
 *   @param int x1 is the x value of our first point
 *   @param int y1 is our y value of our first point
 *   @param int x2 is our x value of our second point
@@ -171,9 +171,9 @@ void simpleDDALine(int x1, int y1, int x2, int y2){
 void bresLine(int x1, int y1, int x2, int y2){
    int oct = 0;
    int xoff = x1;
-   int yoff = y1; 
+   int yoff = y1;
    x2=x2-xoff;
-   y2=y2-yoff; 
+   y2=y2-yoff;
    x1=0;
    y1=0;
    if(y2==0){ if (x1>x2){int t=x1;x1=x2;x2=t;}horizLine(x1+xoff,x2+xoff,y1+yoff); return;}
@@ -185,7 +185,7 @@ void bresLine(int x1, int y1, int x2, int y2){
        sum=x2-x1,
        Dx=2*(x2-x1),
        Dy=2*(y2-y1);
-   for(int i=0; i <= x2-x1; i++){ 
+   for(int i=0; i <= x2-x1; i++){
       switch(oct){
          case 0:pixPlot(( x)+xoff,( y)+yoff);break;
          case 1:pixPlot(( y)+xoff,( x)+yoff);break;
